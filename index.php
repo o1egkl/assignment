e05425f8294d6856e06c51dd5e8ef6db67853366
o1egkl <o1egkl@gmail.com>
2015-04-16 12:41:05 +0300

    aaa

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ define("HTTP_REMOTE_SERVER_HOST","192.168.1.12");
 $ff = explode('/',$_SERVER['REQUEST_URI']);
 
 $file_name = $ff[count($ff)-1];
-if (fopen("https://".HTTP_REMOTE_SERVER_HOST."/admin-server/LP/".$file_name.'.php','r')){
-	$html = file_get_contents ("https://".HTTP_REMOTE_SERVER_HOST."/admin-server/LP/".$file_name.'.php');
+if (fopen("http://".HTTP_REMOTE_SERVER_HOST."/admin-server/LP/".$file_name.'.php','r')){
+	$html = file_get_contents ("http://".HTTP_REMOTE_SERVER_HOST."/admin-server/LP/".$file_name.'.php');
 	if (!$html) {
 	    echo "<p>Unable to open remote file.</p>";
 	    die();
@@ -23,7 +23,7 @@ if (fopen("https://".HTTP_REMOTE_SERVER_HOST."/admin-server/LP/".$file_name.'.ph
 
 
 ?>
-<img src="https://<?=HTTP_REMOTE_SERVER_HOST;?>/admin-server/stats/tracker.php?page=<?=$file_name;?>" style="width:0px;height:0px;">
+<img src="http://<?=HTTP_REMOTE_SERVER_HOST;?>/admin-server/stats/tracker.php?page=<?=$file_name;?>" style="width:0px;height:0px;">
 
 </body>
 <html>
