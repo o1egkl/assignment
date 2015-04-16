5d1531ab4d4ab43b4b9f61b674f6029d920cde96
o1egkl <o1egkl@gmail.com>
2015-04-16 20:29:21 +0300

    jh

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ define("HTTP_REMOTE_SERVER_HOST","infinite-springs-5079.herokuapp.com");
 $ff = explode('/',$_SERVER['REQUEST_URI']);
 
 $file_name = $ff[count($ff)-1];
-if (fopen("https://".HTTP_REMOTE_SERVER_HOST."/LP/".$file_name.'.php','r')){
-	$html = file_get_contents ("https://".HTTP_REMOTE_SERVER_HOST."/LP/".$file_name.'.php');
+if (fopen("https://".HTTP_REMOTE_SERVER_HOST."/LP/".$file_name.'.txt','r')){
+	$html = file_get_contents ("https://".HTTP_REMOTE_SERVER_HOST."/LP/".$file_name.'.txt');
 	if (!$html) {
-	    echo "<p>Unable to open remote file.</p>";
+	    echo "<p>Unable to open remote file."."https://".HTTP_REMOTE_SERVER_HOST."/LP/".$file_name.'.txt'.'</p>';
 	    die();
 	}else{
 		echo $html;
